70da76e89c39b7ac17a4ba56581655ce41c051c3
thoughtgap <3226639+thoughtgap@users.noreply.github.com>
2021-02-14 08:30:26 +0100

    Daily min/max bme280 values

--- temperature-bme280.js
+++ temperature-bme280.js
@@ -9,6 +9,25 @@ var status = {
         pressure: null,
         humidity: null
     },
+    daily: {
+        day: moment(),
+        min: {
+            temperature: null,
+            pressure: null,
+            humidity: null,
+            temperatureTime: null,
+            pressureTime: null,
+            humidityTime: null
+        },
+        max: {
+            temperature: null,
+            pressure: null,
+            humidity: null,
+            temperatureTime: null,
+            pressureTime: null,
+            humidityTime: null
+        }
+    },
     time: null,
     intervalSec: null
 }
@@ -34,6 +53,41 @@ readBME280 = () => {
             status.time = new moment();
             logging.add(`BME280 temperature ${status.values.temperature.toFixed(2)} pressure ${status.values.pressure.toFixed(2)} humidity ${status.values.humidity.toFixed(2)}`);
             logging.thingspeakLog("field1="+status.values.temperature.toFixed(2)+"&field2="+status.values.pressure.toFixed(2)+"&field3="+status.values.humidity.toFixed(2));
+
+            // Daily min/max values
+            if(!moment().isSame(status.daily.day, 'day')) {
+                status.daily.day = moment();
+                status.daily.min = status.values;
+                status.daily.max = status.values;
+            }   
+            //logging.add(`BME280 temperature isSame`);
+            // Min
+            if(!status.daily.min.temperature || status.daily.min.temperature >= status.values.temperature) {
+                status.daily.min.temperature = status.values.temperature;
+                status.daily.min.temperatureTime = moment();
+            }
+            if(!status.daily.min.pressure || status.daily.min.pressure >= status.values.pressure) {
+                status.daily.min.pressure = status.values.pressure;
+                status.daily.min.pressureTime = moment();
+            }
+            if(!status.daily.min.humidity || status.daily.min.humidity >= status.values.humidity) {
+                status.daily.min.humidity = status.values.humidity;
+                status.daily.min.humidityTime = moment();
+            }
+
+            // Max
+            if(!status.daily.max.temperature || status.daily.max.temperature <= status.values.temperature) {
+                status.daily.max.temperature = status.values.temperature;
+                status.daily.max.temperatureTime = moment();
+            }
+            if(!status.daily.max.pressure || status.daily.max.pressure <= status.values.pressure) {
+                status.daily.max.pressure = status.values.pressure;
+                status.daily.max.pressureTime = moment();
+            }
+            if(!status.daily.max.humidity || status.daily.max.humidity <= status.values.humidity) {
+                status.daily.max.humidity = status.values.humidity;
+                status.daily.max.humidityTime = moment();
+            }
 
             if(status.intervalSec) {
                 setTimeout(function erneutLesen() {
