144c8d9f32834ffaff2a7fd911957fe54198b14f
thoughtgap <3226639+thoughtgap@users.noreply.github.com>
2021-02-20 14:19:42 +0100

    Add development of bme280 values over time

--- temperature-bme280.js
+++ temperature-bme280.js
@@ -9,6 +9,19 @@ var status = {
         pressure: null,
         humidity: null
     },
+    time: null,
+    intervalSec: null,
+    compareTo: {
+        temperature: null,
+        pressure: null,
+        humidity: null,
+        time: null,
+        development: {
+            temperature: null,
+            pressure: null,
+            humidity: null
+        }
+    },
     daily: {
         day: moment(),
         min: {
@@ -28,9 +41,9 @@ var status = {
             humidityTime: null
         }
     },
-    time: null,
-    intervalSec: null
 }
+
+var history = [];
 
 var config = {
     port: null
@@ -50,13 +63,87 @@ readBME280 = () => {
             status.values = await sensor.read();
             await sensor.close();
             status.busy = false;
-            status.time = new moment();
+            let now = moment();
+            status.time = now;
             logging.add(`BME280 temperature ${status.values.temperature.toFixed(2)} pressure ${status.values.pressure.toFixed(2)} humidity ${status.values.humidity.toFixed(2)}`);
             logging.thingspeakLog("field1="+status.values.temperature.toFixed(2)+"&field2="+status.values.pressure.toFixed(2)+"&field3="+status.values.humidity.toFixed(2));
 
+            // Minutely history, preserved for one hour
+            const keepEverySec = 60; // How often to preserve a value
+            const keepForMin = 60;   // How long to preserve all values
+
+            // Add value every x seconds
+            logging.add(`BME280 hourly history ${history.length} entries`);
+            
+            if(history.length == 0 || now.diff(history[history.length - 1].time) >= keepEverySec * 1000) {
+                
+                history.push({
+                    time: now,
+                    temperature: status.values.temperature,
+                    pressure: status.values.pressure,
+                    humidity: status.values.humidity
+                });
+                logging.add(`BME280 hourly history adding      --> ${history.length} entries`);
+
+                // Discard values older than x minutes
+                history = history.filter(item => {
+                    let keep = now.diff(item.time) <= (keepForMin * 60 * 1000);
+                    //logging.add(`BME280 hourly history filtering   --> keep=${keep}     ${now.diff(item.time)} <= ${(keepForMin * 60 * 1000)}`);
+                    return keep;
+                });
+                logging.add(`BME280 hourly history removed old --> ${history.length} entries`);
+
+            
+                // Add development of values
+                let development = {
+                    temperature: null,
+                    pressure: null,
+                    humidity: null
+                };
+                let tempAct = Math.round(status.values.temperature * 10) / 10;
+                let tempComp = Math.round(history[0].temperature * 10) / 10;
+                if(tempAct > tempComp) {
+                    development.temperature = "up";
+                }
+                else if(tempAct < tempComp) {
+                    development.temperature = "down";
+                }
+                else if(tempAct == tempComp) {
+                    development.temperature = "same";
+                }
+
+                if(Math.round(status.values.pressure) > Math.round(history[0].pressure)) {
+                    development.pressure = "up";
+                }
+                else if(Math.round(status.values.pressure) < Math.round(history[0].pressure)) {
+                    development.pressure = "down";
+                }
+                else if(Math.round(status.values.pressure) == Math.round(history[0].pressure)) {
+                    development.pressure = "same";
+                }
+
+                if(Math.round(status.values.humidity) > Math.round(history[0].humidity)) {
+                    development.humidity = "up";
+                }
+                else if(Math.round(status.values.humidity) < Math.round(history[0].humidity)) {
+                    development.humidity = "down";
+                }
+                else if(Math.round(status.values.humidity) == Math.round(history[0].humidity)) {
+                    development.humidity = "same";
+                }
+
+                // Only expose the oldest element (max 1h old) to the status object to keep minimal data 
+                status.compareTo = history[0];
+                status.compareTo.development = development;
+
+            }
+            // else {
+            //     logging.add(`BME280 hourly history not adding (${history[history.length - 1].time}ms)`); 
+            // }
+
             // Daily min/max values
-            if(!moment().isSame(status.daily.day, 'day')) {
-                status.daily.day = moment();
+            if(!now.isSame(status.daily.day, 'day')) {
+                status.daily.day = now;
                 status.daily.min = {
                     temperature: null,
                     pressure: null,
@@ -74,33 +161,33 @@ readBME280 = () => {
                     humidityTime: null
                 };
             }   
-            //logging.add(`BME280 temperature isSame`);
+            
             // Min
             if(!status.daily.min.temperature || status.daily.min.temperature >= status.values.temperature) {
                 status.daily.min.temperature = status.values.temperature;
-                status.daily.min.temperatureTime = moment();
+                status.daily.min.temperatureTime = now;
             }
             if(!status.daily.min.pressure || status.daily.min.pressure >= status.values.pressure) {
                 status.daily.min.pressure = status.values.pressure;
-                status.daily.min.pressureTime = moment();
+                status.daily.min.pressureTime = now;
             }
             if(!status.daily.min.humidity || status.daily.min.humidity >= status.values.humidity) {
                 status.daily.min.humidity = status.values.humidity;
-                status.daily.min.humidityTime = moment();
+                status.daily.min.humidityTime = now;
             }
 
             // Max
             if(!status.daily.max.temperature || status.daily.max.temperature <= status.values.temperature) {
                 status.daily.max.temperature = status.values.temperature;
-                status.daily.max.temperatureTime = moment();
+                status.daily.max.temperatureTime = now;
             }
             if(!status.daily.max.pressure || status.daily.max.pressure <= status.values.pressure) {
                 status.daily.max.pressure = status.values.pressure;
-                status.daily.max.pressureTime = moment();
+                status.daily.max.pressureTime = now;
             }
             if(!status.daily.max.humidity || status.daily.max.humidity <= status.values.humidity) {
                 status.daily.max.humidity = status.values.humidity;
-                status.daily.max.humidityTime = moment();
+                status.daily.max.humidityTime = now;
             }
 
             if(status.intervalSec) {
